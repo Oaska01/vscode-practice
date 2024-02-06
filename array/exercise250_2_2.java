b662784aca7f4667dea4cde4c5e476bd1d3fc720
Oaska01 <153800925+Oaska01@users.noreply.github.com>
2024-02-06 12:28:32 +0200

    creating folders

---
+++ array/exercise250_2_2.java
@@ -0,0 +1,35 @@
+package array;
+import java.util.Scanner;;
+public class exercise250_2_2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the size of list1 then fill it : ");
+        int size1 = scan.nextInt();
+        int[] list1 = new int[size1];
+        for(int i = 0; i < list1.length; i++)
+            list1[i] = scan.nextInt();
+    //list2
+        System.out.println("Enter the size of list 2, then fill it : ");
+        int size2 = scan.nextInt();
+        int[] list2 = new int[size2];
+        for(int i = 0; i < list2.length; i++){
+            list2[i] = scan.nextInt();
+        }
+        if(equals(list1, list2)){
+            System.out.println("Identical");
+        }else{
+            System.out.println("Not identical");
+        }
+    }
+    public static boolean equals(int[] list1, int[] list2){
+        if(list1 .length!= list2.length){
+            return false;
+        }
+        for(int i = 0; i < list1.length; i++){
+            if(list1[i] != list2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ array/exercise250_3_array.java
@@ -0,0 +1,10 @@
+package array;
+public class exercise250_3_array {
+    public static void main(String[] args) {
+        System.out.println("Number\t\tSquareRoot");
+        System.out.println("----------------------------");
+        for(int i = 0; i < 20; i++){
+            System.out.println(i + "\t\t" + Math.sqrt(i));
+        }
+    }
+}
